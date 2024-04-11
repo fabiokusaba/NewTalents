65ded7e117a777c6c7c08259a0cef7db393a90f6
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-11 19:09:30 -0300

    Criado teste do método somar

--- TestNewTalents/UnitTest1.cs
+++ TestNewTalents/UnitTest1.cs
@@ -1,11 +1,19 @@
+using NewTalents;
+
 namespace TestNewTalents
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 9)]
+        public void TesteSomar(int val1, int val2, int resultado)
         {
             Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
         }
     }
 }
