3fe6dadf304e78fd72a0759615620caad6bbf854
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-11 19:42:09 -0300

    Implementado o método subtrair

--- NewTalents/Calculadora.cs
+++ NewTalents/Calculadora.cs
@@ -15,7 +15,7 @@ namespace NewTalents
 
         public int subtrair(int val1, int val2)
         {
-            return 0;
+            return val1 - val2;
         }
 
         public int multiplicar(int val1, int val2)
