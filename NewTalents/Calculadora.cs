03373a02a038e0a5b7ecb850d03c73209a304923
Fabio Kusaba <fabinhokb44@gmail.com>
2024-04-11 19:52:34 -0300

    Criado a propriedade listaHistorico e refatorado os métodos somar, subtrair, multiplicar e dividir

--- NewTalents/Calculadora.cs
+++ NewTalents/Calculadora.cs
@@ -8,24 +8,47 @@ namespace NewTalents
 {
     public class Calculadora
     {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+            listaHistorico = new List<string>();
+        }
+
         public int somar(int val1, int val2)
         {
-            return val1 + val2;
+            int resultado = val1 + val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public int subtrair(int val1, int val2)
         {
-            return val1 - val2;
+            int resultado = val1 - val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public int multiplicar(int val1, int val2)
         {
-            return val1 * val2;
+            int resultado = val1 * val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado; 
         }
 
         public int dividir(int val1, int val2)
         {
-            return val1 / val2;
+            int resultado = val1 / val2;
+
+            listaHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public List<string> historico()
